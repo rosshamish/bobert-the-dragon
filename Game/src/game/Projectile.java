c33062862bf7f5bc1cc9d7bd6f6f31da38bd1fdc
Ross Anderson <ross.anderson87@gmail.com>
2012-03-19 23:22:16 -0600

    First --force push

--- Game/src/game/Projectile.java
+++ Game/src/game/Projectile.java
@@ -7,8 +7,6 @@ package game;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Rectangle;
-import java.io.File;
-import java.io.IOException;
 import javax.swing.ImageIcon;
 
 /**
